287bab7476a9ac267837b035d9cc91aeca1c5296
Tejus  S <tejus@Tejuss-Air.wv.cc.cmu.edu>
2017-04-30 20:21:53 -0400

    some changes to the script

--- SerialGUI/serial_gui.py
+++ SerialGUI/serial_gui.py
@@ -21,7 +21,7 @@ m = 'o'
 
 ax.set_xlim3d(0, 12)
 ax.set_ylim3d(0,12)
-ax.set_zlim3d(-6,6)
+ax.set_zlim3d(0,12)
 ax.set_xlabel('X axis')
 ax.set_ylabel('Y axis')
 ax.set_zlabel('Z axis')
@@ -38,33 +38,43 @@ with serial.Serial('/dev/tty.usbmodem1411', 115200, timeout=1) as ser:
         line3 = ser.readline()
         line4 = ser.readline()
 
-        # print line1,
-        # print line2,
-        # print line3,
-        # print line4
+        print line1,
+        print line2,
+        print line3,
+        print line4
         try:
             line1.split(' ') and line2.split(' ') and line3.split(' ') and line4.split(' ')
             id1, r1 = line1.split(' ')
             id2, r2 = line2.split(' ')
             id3, r3 = line3.split(' ')
             id4, r4 = line4.split(' ')
-            # print r1, 
-            # print id1
-            # print r2, 
-            # print id2
-            # print r3, 
-            # print id3
-            # print r4,
-            # print id4
+            print r1, 
+            print id1
+            print r2, 
+            print id2
+            print r3, 
+            print id3
+            print r4,
+            print id4
+            
             if((len(id1) == 8) and (len(id2) == 8) and (len(id3) == 8) and (len(id4) == 8)):
                 try:
-                    float(r1) and float(r2) and float(r3) and float(r4)
+                    float(r1) and float(r2) and float(r3) #and float(r4)
                     P=lx.Project(mode='3D', solver='LSE')
                     P.add_anchor('FFFFFAFF', (0, 0, 0))
-                    P.add_anchor('FFFFFCFF', (12,5,3))
-                    P.add_anchor('FFFFFBFF', (10, 0, 0))
-                    P.add_anchor('FFFFFDFF', (8, 5, -2))
+                    P.add_anchor('FFFFFBFF', (12.35, 0 , 0.0))
+                    P.add_anchor('FFFFFCFF', (10.43, 5.73, 1.2))
+                    P.add_anchor('FFFFFDFF', (9.31, 5.73, -0.72))
+                    
+                    # P.add_anchor('FFFFFAFF', (0, 0, 0))
+                    # P.add_anchor('FFFFFBFF', (12, 0 , 0))
+                    # P.add_anchor('FFFFFCFF', (10, 5, 3))
+                    # P.add_anchor('FFFFFDFF', (8, 5, -2))
                     t,label=P.add_target()
+                    r1 = float(r1)
+                    r2 = float (r2)
+                    r3 = float(r3)
+                    r4 = float (r4)
                     t.add_measure(id1, r1)
                     t.add_measure(id2, r2)
                     t.add_measure(id3, r3)
@@ -73,15 +83,15 @@ with serial.Serial('/dev/tty.usbmodem1411', 115200, timeout=1) as ser:
                     P.solve()
                     # end = time.time()
                     # print (end - start)
-                    print r1, 
-                    print id1
-                    print r2, 
-                    print id2
-                    print r3, 
-                    print id3
-                    print r4,
-                    print id4
-                    print "************"
+                    # print r1, 
+                    # print id1
+                    # print r2, 
+                    # print id2
+                    # print r3, 
+                    # print id3
+                    # print r4,
+                    # print id4
+                    # print "************"
                     # plt.gcf().clear()
                     print round(t.loc.x, 3),
                     print round(t.loc.y, 3),
@@ -91,8 +101,8 @@ with serial.Serial('/dev/tty.usbmodem1411', 115200, timeout=1) as ser:
                     zs = round(t.loc.z, 3)
                     plt.cla()
                     ax.set_xlim3d(0, 12)
-                    ax.set_ylim3d(0,5)
-                    ax.set_zlim3d(-6,6)
+                    ax.set_ylim3d(0,12)
+                    ax.set_zlim3d(0,12)
                     ax.set_xlabel('X axis')
                     ax.set_ylabel('Y axis')
                     ax.set_zlabel('Z axis')
